aadc2fa06f5221f2344da0f3e7fe7fce42b380bf
cjdsellers <chris@cjdsellers.io>
2024-03-24 11:14:17 +0000

    Update gh-pages to output generated at 181efe5

--- nightly/core/nautilus_common/msgbus/sidebar-items.js
+++ nightly/core/nautilus_common/msgbus/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["is_matching"],"struct":["BusMessage","MessageBus","Subscription"]};
+window.SIDEBAR_ITEMS = {"constant":["CLOSE_TOPIC"],"fn":["is_matching"],"struct":["BusMessage","MessageBus","Subscription"]};
